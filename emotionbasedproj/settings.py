797ea02f4a78446448cd97d5859b87a982cb9b1d
MdMusharrafKhan <63579131+MdMusharrafKhan@users.noreply.github.com>
2025-03-14 00:19:27 +0530

    Update settings.py
    Allowed Hosts Changed.

--- emotionbasedproj/settings.py
+++ emotionbasedproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h^h)^1tayejf(vsevqk(si@kk6$6y%pg!ssl7q8zyt8s1m=+cc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
